db8a139f40b969c1422b43ebb2ebeb73c3346f12
Antonio Ladeia <contato@antonioladeia.com>
2018-06-28 07:25:55 -0300

    fix crawler for new html

--- courses/courses/spiders/udacity.py
+++ courses/courses/spiders/udacity.py
@@ -5,16 +5,16 @@ import scrapy
 class UdacitySpider(scrapy.Spider):
 
     name = 'udacity'
-    start_urls = ['http://br.udacity.com/courses/all/']
+    start_urls = ['https://br.udacity.com/courses/all/']
 
     def parse(self, response):
-        divs = response.xpath("/html/body/ir-root/ir-content/ir-course-catalog-old/section/div/div/div/div[1]")
+        divs = response.xpath("/html/body/ir-root/ir-content/ir-course-catalog-old/section/div/div/div/div[1]/div")
         for div in divs:
-            link = div.xpath('.//h3')
+            link = div.xpath('.//h3/a')
             title = link.xpath('./text()').extract_first()
             href = link.xpath('./@href').extract_first()
-            image = div.xpath('.//img[contains(@class, "img-responsive")]').extract_first()
-            description = div.xpath('.//div[2]/div[2]/text()').extract_first()
+            image = div.xpath('.//img[contains(@class, "img-responsive")]/@src').extract_first()
+            description = div.xpath('.//div[2]/div[2]/span/text()').extract_first()
             yield {
                 'title': title,
                 'url': href,
